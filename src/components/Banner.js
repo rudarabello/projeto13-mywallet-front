58c72d7b9e3ab8498564575291e0c3d5d02459af
rudarabello <ruda.rabello.silva@gmail.com>
2022-10-12 10:53:27 -0300

    feat: change position of images and types of img

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -9,24 +9,25 @@ const Banner = () => {
         <Box>
             <Carousel autoPlay={true} infiniteLoop={true} interval={3500} showThumbs={false} showStatus={false}>
                 <div>
-                    <img alt="celular sem fundo" src="https://nubank.com.br/images-cms/1650480044-digital-account-phone-main-screen-md-2x.png"></img> 
+                    <img alt="celular sem fundo" src="https://iili.io/S4BkFf.png"></img> 
                 </div>
                 <div>
                     <img alt="deitado com celular na mao"src="https://nubank.com.br/images-cms/1650480415-man-sitting-with-phone-xs-3x.jpg"></img>
                 </div>
                 <div>
-                    <img alt="segurando o celular" src="https://iili.io/S4BkFf.png"></img>
+                    <img alt="segurando o celular" src="https://www.milk-education.co.uk/wp-content/uploads/2017/07/Teacher-Holding-Phone.png"></img>
                 </div>
             </Carousel>
         </Box>
     )
 }
 
+
 export default Banner
 
 const Box = styled.div`
     display: flex;
-    img{
-    height: 100%;
-}
+    height: 30%;
+    align-items: center;
+    justify-content: center;
 `;
